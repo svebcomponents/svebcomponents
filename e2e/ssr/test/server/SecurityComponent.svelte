c5cb065fd61536ff9a20277e8a4aba797d452b3f
Theodor Steiner <theosteiner@gmail.com>
2026-06-27 16:32:17 +0900

    test ssr xss escaping

---
+++ e2e/ssr/test/server/SecurityComponent.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  // import & register web component
+  import "../../dist/client/index.js";
+
+  interface Props {
+    attributePayload?: string;
+    content: string;
+    title: string;
+  }
+
+  let { title, attributePayload = title, content }: Props = $props();
+</script>
+
+<simple-component {title} data-test={attributePayload}>
+  {content}
+</simple-component>
